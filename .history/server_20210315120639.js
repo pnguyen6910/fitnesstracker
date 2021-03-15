f2c941ed2bbea9ee2c9fa983a9f76ce19542bef8
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-03-15 12:07:05 -0500

    setting up server js

---
+++ .history/server_20210315120639.js
@@ -0,0 +1,7 @@
+//npm packages
+const express = require("express")
+const mongoose = require("mongoose")
+const path = require("path")
+const db = require("./models")
+
+const PORT = process.env.PORT || 8080
